d453c161f9e4fff7582a777ab6f59d5cf6e0feea
Arindam Podder <arindam.podder@sevyamm.com>
2024-04-01 00:45:13 +0530

    ldap configure

---
+++ src/main/java/arindam_boot/boot_ldap_authentication/BootLdapAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package arindam_boot.boot_ldap_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLdapAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLdapAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/arindam_boot/boot_ldap_authentication/BootLdapAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package arindam_boot.boot_ldap_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLdapAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
